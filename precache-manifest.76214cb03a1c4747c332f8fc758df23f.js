0119863796945196022c8cf030af729ab6263128
Sebastian Evert <sebastian.evert@titantech.cl>
2019-04-15 23:35:25 -0400

    Updates

---
+++ precache-manifest.76214cb03a1c4747c332f8fc758df23f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/02_react_tictactoe/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "4111c06f05a9aa50e59a",
+    "url": "/02_react_tictactoe/static/js/runtime~main.403ce03f.js"
+  },
+  {
+    "revision": "6f8936fd7b580ea37e7b",
+    "url": "/02_react_tictactoe/static/js/main.da3485e1.chunk.js"
+  },
+  {
+    "revision": "f44e8d71996b6ad2a746",
+    "url": "/02_react_tictactoe/static/js/2.a6bbc099.chunk.js"
+  },
+  {
+    "revision": "6f8936fd7b580ea37e7b",
+    "url": "/02_react_tictactoe/static/css/main.508d62e5.chunk.css"
+  },
+  {
+    "revision": "f44e8d71996b6ad2a746",
+    "url": "/02_react_tictactoe/static/css/2.3ebf90cd.chunk.css"
+  },
+  {
+    "revision": "a01f4b56c8aaa1ceae0203d89f9c69d9",
+    "url": "/02_react_tictactoe/index.html"
+  }
+];
